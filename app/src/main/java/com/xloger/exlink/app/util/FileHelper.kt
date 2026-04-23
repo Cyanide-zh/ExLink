b6fd88347abb980c18cab9a18c69e5b10dba218e
Cyanide-zh <111837046+Cyanide-zh@users.noreply.github.com>
2026-04-23 21:14:17 +0800

    Update FileHelper.kt

--- app/src/main/java/com/xloger/exlink/app/util/FileHelper.kt
+++ app/src/main/java/com/xloger/exlink/app/util/FileHelper.kt
@@ -13,20 +13,13 @@ import java.nio.charset.Charset
  */
 object FileHelper {
 
-    //fun getJsonPath() : String {
-       // val sdPath = "/sdcard"
-        //val dir = File(sdPath, "ExLink")
-        //if (!dir.exists()) {
-        //    dir.mkdir()
-      //  }
-       // return dir.path + File.separator + "exlink.json"
-   // } //原版不符合Android规范
-    // 需要将 context 传入或通过静态方式获取
-    fun getJsonPath(context: Context): String {
-        // 路径：/data/user/0/com.xloger.exlink.app/files/exlink.json
-        val dir = context.filesDir // 获取应用的私有 files 目录
-        val file = File(dir, "exlink.json")
-        return file.path
+    fun getJsonPath() : String {
+        val sdPath = "/sdcard"
+        val dir = File(sdPath, "Documents")
+        if (!dir.exists()) {
+            dir.mkdir()
+        }
+        return dir.path + File.separator + "exlink.json"
     }
 
     fun readFile(path: String = getJsonPath()): String {
